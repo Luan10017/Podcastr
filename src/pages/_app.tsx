2b401444c804802d5fa07512bc5353377a219135
Luan <luan10017@hotmail.com>
2021-04-21 13:51:59 -0300

    First commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import '../styles/global.scss'
+
+import { Header } from '../componentes/Header'
+import { Player } from '../componentes/Player'
+
+import styles from '../styles/app.module.scss'
+
+function MyApp({ Component, pageProps }) {
+ return(
+    <div className={styles.wrapper}>
+      <main>
+        <Header/>
+        <Component {...pageProps} />
+      </main>
+      <Player/>
+    </div>
+  )
+}
+
+export default MyApp
